e05a86bbbbbb1aecf2e56f653c4521c56c3be3e1
tokogit2021 <rodrigue.tokot@gmail.com>
2024-05-23 16:42:52 -0500

    add old/new endpoint

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/hello-world", handleHelloWorld)
 	http.HandleFunc("/health", handleHealth)
-	http.HandleFunc("/new-endpoint", handleNewEndpoint)
+	http.HandleFunc("/old-endpoint", handleOldEndpoint)
 
 	addr := "localhost:8000"
 	log.Printf("Listening on %s ...", addr)
@@ -36,7 +36,7 @@ func handleHealth(writer http.ResponseWriter, request *http.Request) {
 	writeResponse(writer, "Ok!")
 }
 
-func handleNewEndpoint(writer http.ResponseWriter, request *http.Request) {
+func handleOldEndpoint(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != "GET" {
 		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
